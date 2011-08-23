840507c6650897890a49843b0c1f26d9b954399f
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2011-08-23 15:07:17 +0200

    generating the plugin

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_time_tracker do
+  name 'Redmine Time Tracker plugin'
+  author 'BadrIT'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://www.badrit.com'
+  
+end
