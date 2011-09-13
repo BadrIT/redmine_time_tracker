5c28408eda46390d02d9040530fd91594bd2119f
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2011-09-13 16:47:25 +0200

    handling start_time as an TimeEntry attribute

--- lib/time_tracker/time_entry_patch.rb
+++ lib/time_tracker/time_entry_patch.rb
@@ -27,6 +27,20 @@ module TimeTracker
       def update_issue_history
         self.issue.check_history_entries
       end
+      
+      def attributes=(new_attributes, guard_protected_attributes = true)
+        start_time = new_attributes[:start_time]
+        
+        if start_time
+          start_time_custom_field = TimeEntryCustomField.find_by_name 'Start time'
+          self.custom_field_values = {start_time_custom_field.id => start_time}
+        end
+        
+        new_attributes.delete :start_time
+        
+        super(new_attributes, guard_protected_attributes = true)
+      end
+      
     end
   end
 end
