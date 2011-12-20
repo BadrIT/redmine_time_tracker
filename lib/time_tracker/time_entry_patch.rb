1b4ddbb0851f61afc841d5bbce677835b14fb95f
Mohamed Magdy <mohamed.magdy@badrit.com>
2011-12-20 13:47:56 +0200

    Fixes #1174 Fixing the delete and edity issue exceptions

--- lib/time_tracker/time_entry_patch.rb
+++ lib/time_tracker/time_entry_patch.rb
@@ -29,16 +29,18 @@ module TimeTracker
       end
       
       def attributes=(new_attributes, guard_protected_attributes = true)
-        start_time = new_attributes[:start_time]
+        if new_attributes
+          start_time = new_attributes[:start_time]
         
-        if start_time
-          start_time_custom_field = TimeEntryCustomField.find_by_name 'Start time'
-          self.custom_field_values = {start_time_custom_field.id => start_time}
+          if start_time
+            start_time_custom_field = TimeEntryCustomField.find_by_name 'Start time'
+            self.custom_field_values = {start_time_custom_field.id => start_time}
+          end
+          
+          new_attributes.delete :start_time
+          
+          super(new_attributes, guard_protected_attributes = true)          
         end
-        
-        new_attributes.delete :start_time
-        
-        super(new_attributes, guard_protected_attributes = true)
       end
       
     end
