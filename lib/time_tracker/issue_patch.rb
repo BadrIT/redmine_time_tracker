8998b8e14fd4e80ebfff499ef49a7e01f52f528e
Mahmoud <mahmoud.khaled@badrit.com>
2013-01-01 15:16:54 +0200

    update issue active named scope

--- lib/time_tracker/issue_patch.rb
+++ lib/time_tracker/issue_patch.rb
@@ -6,6 +6,8 @@ module TimeTracker
 				unloadable # Send unloadable so it will not be unloaded in development
 				
 				include InstanceMethods
+
+				scope :active, lambda { |*args| {:conditions => ["status_id in (?)", IssueStatus.where('is_closed = ?', false)]} }
 				
 		  end
 		end
